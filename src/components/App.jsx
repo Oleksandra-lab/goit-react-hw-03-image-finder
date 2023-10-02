92dec1e9d87a4724b842dbab7cab55d12aa1eac6
Oleksandra-lab <127051632+Oleksandra-lab@users.noreply.github.com>
2023-10-02 08:22:48 +0300

    Loader is added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
 import { StyledApp } from './App.styled';
+import {Blocks} from 'react-loader-spinner'
 import { getImages } from 'Services/getImages';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -20,8 +21,8 @@ export class App extends Component {
   };
 
   componentDidUpdate(prevProps, prevState) {
-    const {query, page} = this.state;
-    if(prevState.query !== query || prevState.page !== page){
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
       this.fetchAllImages();
     }
   }
@@ -29,42 +30,62 @@ export class App extends Component {
   fetchAllImages = async () => {
     const { query, page } = this.state;
     try {
-      const {hits, totalHits} = await getImages(query, page);
-      this.setState(prevState => ({images: [...prevState.images, ...hits], totalResults: totalHits}) );
-    } catch (error) {this.setState({error: error.message})}
+      const { hits, totalHits } = await getImages(query, page);
+      this.setState(prevState => ({
+        images: [...prevState.images, ...hits],
+        totalResults: totalHits,
+      }));
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
   };
 
-  onHandleSubmit = (value) =>{
-    this.setState({ query: value, images: [],
+  onHandleSubmit = value => {
+    this.setState({
+      query: value,
+      images: [],
       totalResults: 0,
       page: 1,
       isLoading: false,
-      error: null,});
+      error: null,
+    });
+  };
 
+  onLoadMoreClick = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
 
-  }
-
-  onLoadMoreClick = () =>{
-    this.setState(prevState  => ({page: prevState.page + 1}))
-
-  }
-  
-  onToggleModal = (largeImage) => {
-    this.setState( ({isOpenModal: true, modalImg: largeImage}));
-  }
+  onToggleModal = largeImage => {
+    this.setState({ isOpenModal: true, modalImg: largeImage });
+  };
 
   onCloseModal = () => {
-    this.setState( ({isOpenModal: false, modalImg: ''}));
-  }
+    this.setState({ isOpenModal: false, modalImg: '' });
+  };
   render() {
-    const {images, totalResults, isOpenModal, modalImg} = this.state;
-    console.log(this.state.modalImg);
+    const { images, totalResults, isOpenModal, modalImg } = this.state;
     return (
       <StyledApp>
-        <Searchbar onFormSubmit={this.onHandleSubmit}/>
-        <ImageGallery images={images} onToggleModal={this.onToggleModal}/>
-        {images.length<totalResults && <Button onClick={this.onLoadMoreClick}>Load more</Button>}
-        {isOpenModal&&<Modal modalImg={modalImg} onCloseModal={this.onCloseModal}/>}
+        <Searchbar onFormSubmit={this.onHandleSubmit} />
+        <ImageGallery images={images} onToggleModal={this.onToggleModal} />
+        {images.length < totalResults && (
+          <Button onClick={this.onLoadMoreClick}>Load more</Button>
+        )}
+        {isOpenModal && (
+          <Modal modalImg={modalImg} onCloseModal={this.onCloseModal} />
+        )}
+        {this.state.isLoading && (
+          <div>
+            <Blocks
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="blocks-loading"
+              wrapperStyle={{}}
+              wrapperClass="blocks-wrapper"
+            />
+          </div>
+        )}
       </StyledApp>
     );
   }
